5509ae9f014e6eb439b1d14388ce0fb59a8ebdf3
Dario <dario.defrancisci92@gmail.com>
2021-07-27 13:10:40 +0200

    update - correzioni

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@
 var min = 1;
 var max = 100;
 var bombe = []; 
+var numBombe = 16;
 
-//16 volte
+///creo array con le mie bombe - 16 volte
 let text = "";
 let i = 0;
-while (i < 16) {
+while (i < numBombe) {
     var numeroPc = rndNumber(min, max);
     bombe.push (numeroPc);
     if(userNumber =  numeroPc) {
@@ -20,21 +21,45 @@ while (i < 16) {
       i++;
 }
 
+//creo array vuoto che conterrà i numeri scelti dall'utente
+
+let numeri_utenti = [];
+let conteggio = 0;
+let possibilita = max - numBombe;
+
+while (numeri_utenti.length < possibilita && (controllo_bombe(numeri_utenti, userNumber)) != true) {
+
+    //TODO: richiedo all'utente un numero incluso tra 1 e max
+    userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
+}   
+
+//SEZIONE FUNZIONI:
+
 //Funzione - Numero Casuale
 function rndNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 console.log(bombe);
 
+//Funzione controllo sulle bombe
+function controllo_bombe(bombe, userNumber) {
+    var find = false;
+    for (let i = 0; i < bombe.length; i++) {
+        if (bombe[i] == userNumber) {
+            find = true;
+        }
+    }
+    return find; 
+} 
 //2) I numeri non possono essere duplicati.
 
 
 //3) In seguito deve chiedere all’utente (100 - 16(ovvero 84)) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //Inserire 84 numeri 
 for (i = 1; i <= 2; i++) {
-    userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
-    //console.log('numero utente ' + userNumber);
-    //document.write('numero utente ' + userNumber + '<br>' );
+    //userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
+    console.log('numero utente ' + userNumber);
+    document.write('numero utente ' + userNumber + '<br>' );
 } 
 //4) L’utente non può inserire più volte lo stesso numero.
 
