b3e57bfecbcaaa93f0fd170602584f2d474d8332
Dario <dario.defrancisci92@gmail.com>
2021-07-27 15:18:33 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 //ESERCIZIO CAMPO MINATO:
 //1) Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 
-//variabili
+//VARIABILI
 var min = 1;
 var max = 100;
-var bombe = []; 
+var lista_bombe = []; 
 var numBombe = 16;
 
-///creo array con le mie bombe - 16 volte
+///creo array con le mie bombe : 16 bombe
 let text = "";
 let i = 0;
 while (i < numBombe) {
     var numeroPc = rndNumber(min, max);
-    bombe.push (numeroPc);
+    lista_bombe.push (numeroPc);
     if(userNumber =  numeroPc) {
         result = false;
     }   else {
@@ -20,32 +20,31 @@ while (i < numBombe) {
     }
       i++;
 }
-
+/*
 //creo array vuoto che conterrà i numeri scelti dall'utente
-
 let numeri_utenti = [];
 let conteggio = 0;
 let possibilita = max - numBombe;
 
 while (numeri_utenti.length < possibilita && (controllo_bombe(numeri_utenti, userNumber)) != true) {
 
-    //TODO: richiedo all'utente un numero incluso tra 1 e max
+    //TODO: richiedo all'utente un numero incluso tra 1 e 100
     userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
 }   
-
+*/
 //SEZIONE FUNZIONI:
 
 //Funzione - Numero Casuale
 function rndNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-console.log(bombe);
+console.log(lista_bombe);
 
 //Funzione controllo sulle bombe
-function controllo_bombe(bombe, userNumber) {
+function controllo_bombe(lista_bombe, userNumber) {
     var find = false;
-    for (let i = 0; i < bombe.length; i++) {
-        if (bombe[i] == userNumber) {
+    for (let i = 0; i < lista_bombe.length; i++) {
+        if (lista_bombe[i] == userNumber) {
             find = true;
         }
     }
@@ -55,17 +54,18 @@ function controllo_bombe(bombe, userNumber) {
 
 
 //3) In seguito deve chiedere all’utente (100 - 16(ovvero 84)) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
 //Inserire 84 numeri 
-for (i = 1; i <= 2; i++) {
-    //userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
+for (i = 1; i <= 2; i++) {//corregere 2 con 84
+    userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
     console.log('numero utente ' + userNumber);
     document.write('numero utente ' + userNumber + '<br>' );
 } 
 //4) L’utente non può inserire più volte lo stesso numero.
 
 //5) Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-//CONDIZIONE " IF ":
 
+//CONDIZIONE " IF ":
 if (userNumber ==  numeroPc) {
     console.log("Giocatore perde!");
     
