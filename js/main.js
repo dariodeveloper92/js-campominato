70ef51c2d9dea6da7af4180204d005c4d2f6c38e
Dario <dario.defrancisci92@gmail.com>
2021-07-27 16:19:10 +0200

    utlimata struttura esercizio

--- js/main.js
+++ js/main.js
@@ -13,25 +13,15 @@ let i = 0;
 while (i < numBombe) {
     var numeroPc = rndNumber(min, max);
     lista_bombe.push (numeroPc);
-    if(userNumber =  numeroPc) {
+    console.log(lista_bombe);
+    /*if(userNumber ==  numeroPc) {
         result = false;
     }   else {
         result = true;
-    }
+    }*/
       i++;
 }
-/*
-//creo array vuoto che conterrà i numeri scelti dall'utente
-let numeri_utenti = [];
-let conteggio = 0;
-let possibilita = max - numBombe;
 
-while (numeri_utenti.length < possibilita && (controllo_bombe(numeri_utenti, userNumber)) != true) {
-
-    //TODO: richiedo all'utente un numero incluso tra 1 e 100
-    userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
-}   
-*/
 //SEZIONE FUNZIONI:
 
 //Funzione - Numero Casuale
@@ -50,63 +40,6 @@ function controllo_bombe(lista_bombe, userNumber) {
     }
     return find; 
 } 
-//2) I numeri non possono essere duplicati.
-
-
-//3) In seguito deve chiedere all’utente (100 - 16(ovvero 84)) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-//Inserire 84 numeri 
-for (i = 1; i <= 2; i++) {//corregere 2 con 84
-    userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100'));
-    console.log('numero utente ' + userNumber);
-} 
-//4) L’utente non può inserire più volte lo stesso numero.
-
-//5) Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-
-//CONDIZIONE " IF ":
-if (userNumber ==  numeroPc) {
-    console.log("Giocatore perde!");
-    
-}
-else {
-    console.log("Giocatore vince!");
-}
-
-//CONDIZIONE " SWITCH ":
-/*
-var operazione = risultato;
-var risultato = 0;
-
-switch(operazione) {
-    case 'perso':
-        risultato = userNumber =  numeroPc + 'hai perso!';
-        break;
-    case 'vinto':
-        risultato = userNumber != numeroPc + 'hai vinto!';
-        break;
-    default:
-        risultato = 'hai perso!' || 'hai vinto!';
-}
-
-console.log('Il risultato è ' + risultato);
-*/
-
-//6) La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-var numVietato = numeroPc;
-
-if (numVietato) {
-    console.log("Giocatore perde!");
-    
-}
-else {
-    console.log("Giocatore vince!");
-}
-
-//7) Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-
-
 //PARTE GRAFICA------------------------------------------------------------------------------------------------------------------------
 
 function creaCampo(celle) {
@@ -126,10 +59,16 @@ function creaCampo(celle) {
 document.getElementById('campo').addEventListener('click',
     function(e) { //e = event
         console.log(e.target.dataset.cella);
-        let element = document.querySelectorAll("data-cella='" + e.target.dataset.cella + "']"); 
+        console.log(lista_bombe.includes(e.target.dataset.cella));
+        console.log(lista_bombe);
+        if (lista_bombe.includes(parseInt(e.target.dataset.cella))) {
+            alert('hai perso!');
+        
+        let element = document.querySelectorAll("[data-cella='" + e.target.dataset.cella + "']"); 
         console.log(element[0]);
         element[0].classList.add("red");
     }
+    }
 )
 
-creaCampo(98);
+creaCampo(100);
